afcda70cf6af016b551f0b6fc57806c830abb18c
ivancapobianco <ivan.capobianco@yahoo.it>
2022-10-03 22:51:45 +0200

    js

--- static/js/sidebar_activepage.js
+++ static/js/sidebar_activepage.js
@@ -2,6 +2,7 @@ const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll('.navigation ul li a').forEach(link => {
   if(link.href.includes(`${activePage}`)){
     link.closest("li").classList.add('activelinknav');
+    link.closest("li").querySelector(".title").classList.add('active');
     console.log(link);
   }
 })
