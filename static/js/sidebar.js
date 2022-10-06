a05576a3eac5dd49b348e8e4806f1dc846c94efa
ivancapobianco <ivan.capobianco@yahoo.it>
2022-10-06 14:48:24 +0200

    sidebar animation

---
+++ static/js/sidebar.js
@@ -0,0 +1,27 @@
+// MenuToggle
+        let toggle = document.querySelector('.toggle');
+        let navigation = document.querySelector('.navigation');
+        let navtitle = document.querySelector('.title');
+        let topbar = document.querySelector('.topbar');
+        let main = document.querySelector('.main');
+        let hometable = document.querySelector('.hometable');
+
+
+        // navigation.onmouseenter
+        toggle.onclick = function(){
+            navigation.classList.toggle('active');
+            navtitle.classList.toggle('active');
+            topbar.classList.toggle('active');
+            hometable.classList.toggle('active');
+            main.classList.toggle('active');
+
+
+        }
+
+        //navigation.onmouseleave = function(){
+        //    navigation.classList.remove('active');
+        //    navtitle.classList.remove('active');
+        //    topbar.classList.remove('active');
+        //    hometable.classList.remove('active');
+        //    main.classList.remove('active');
+        //}
--- static/js/sidebar_activepage.js
+++ static/js/sidebar_activepage.js
@@ -3,6 +3,5 @@ const navLinks = document.querySelectorAll('.navigation ul li a').forEach(link =
   if(link.href.includes(`${activePage}`)){
     link.closest("li").classList.add('activelinknav');
     link.closest("li").querySelector(".title").classList.add('active');
-    console.log(link);
   }
 })
