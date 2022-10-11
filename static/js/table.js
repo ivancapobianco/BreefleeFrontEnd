8b48c8e570b7822490b319c475f7dc6736651c06
ivancapobianco <ivan.capobianco@yahoo.it>
2022-10-11 16:15:45 +0200

    table sort

--- static/js/table.js
+++ static/js/table.js
@@ -24,7 +24,7 @@
         var th = ''
 
         for (var i = 0; i < ar.length; i++){
-			var row = `<th data-column="${ar[i]}" data-order="desc">${ar[i].charAt(0).toUpperCase()+ar[i].slice(1)} &#9650</th>`
+			var row = `<th data-column="${ar[i]}" data-order="desc">${ar[i].charAt(0).toUpperCase()+ar[i].slice(1)}</th>` //&#9650
 		    th += row
         }
 
@@ -55,26 +55,51 @@
 
 
 //sort table columns
-    	$('th').on('click', function(){
-		var column = $(this).data('column')
-		var order = $(this).data('order')
-		var text = $(this).html()
-		text = text.substring(0, text.length - 1)
 
-		if(order == 'desc'){
-			$(this).data('order', "asc")
-			myArray = myArray.sort((a,b) => a[column] > b[column] ? 1 : -1)
-			text += '&#9660'
+/**
+ * Sorts a HTML table.
+ *
+ * @param {HTMLTableElement} table The table to sort
+ * @param {number} column The index of the column to sort
+ * @param {boolean} asc Determines if the sorting will be in ascending
+ */
+function sortTableByColumn(table, column, asc = true) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll("tr"));
 
-		}else{
-			$(this).data('order', "desc")
-			myArray = myArray.sort((a,b) => a[column] < b[column] ? 1 : -1)
-			text += '&#9650'
+    // Sort each row
+    const sortedRows = rows.sort((a, b) => {
+        const aColText = a.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+        const bColText = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
 
-		}
-		$(this).html(text)
-		buildTable(myArray)
-	})
+        return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+    });
+
+    // Remove all existing TRs from the table
+    while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
+    }
+
+    // Re-add the newly sorted rows
+    tBody.append(...sortedRows);
+
+    // Remember how the column is currently sorted
+    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-asc", asc);
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-desc", !asc);
+}
+
+document.querySelectorAll(".table-sortable th").forEach(headerCell => {
+    headerCell.addEventListener("click", () => {
+        const tableElement = headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+    });
+});
+
 
 
 
@@ -84,3 +109,28 @@ $(document).ready(function () {
         window.location.href = this.dataset.href;
     });
 });
+
+
+
+
+//sort table columns ALTERNATIV
+    	//$('th').on('click', function(){
+		//var column = $(this).data('column')
+		//var order = $(this).data('order')
+		//var text = $(this).html()
+		//text = text.substring(0, text.length - 1)
+
+		//if(order == 'desc'){
+		//	$(this).data('order', "asc")
+		//	myArray = myArray.sort((a,b) => a[column] > b[column] ? 1 : -1)
+		//	text += '&#9660'
+
+		//}else{
+		//	$(this).data('order', "desc")
+		//	myArray = myArray.sort((a,b) => a[column] < b[column] ? 1 : -1)
+		//	text += '&#9650'
+
+		//}
+		//$(this).html(text)
+		//buildTable(myArray)
+	//})
