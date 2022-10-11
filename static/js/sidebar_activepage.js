5827b6f86a83c89fa9bef149993cf72a5febf480
ivancapobianco <ivan.capobianco@yahoo.it>
2022-10-11 21:46:37 +0200

    patient menu disabled

--- static/js/sidebar_activepage.js
+++ static/js/sidebar_activepage.js
@@ -1,12 +1,12 @@
 const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll('.navigation ul li a').forEach(link => {
 
-    if(link.href.includes(`${activePage}`)){
+    if (link.href.includes(`${activePage}`)) {
     link.parentElement.classList.add('activelinknav');
     link.parentElement.querySelector(".title").classList.add('active');
   }
 
-   else if(link.href == "http://localhost:63342/pythonProject/static/000") {
+   else if (link.href == "http://localhost:63342/pythonProject/static/000") {
     link.parentElement.classList.add('passive');
    }
 
