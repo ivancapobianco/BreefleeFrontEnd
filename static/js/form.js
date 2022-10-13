57a149b69daefbe5233c6ddc14f12cb4b5bd5075
ivancapobianco <ivan.capobianco@yahoo.it>
2022-10-13 08:29:23 +0200

    form save

--- static/js/form.js
+++ static/js/form.js
@@ -1,7 +1,53 @@
-$('input[type="radio"]').change(function(){
-    if($(this).is(":checked")) {
-        $(this).parent('label').addClass("checked");
+//$('input[type="radio"]').change(function(){
+//    if($(this).is(":checked")) {
+//        $(this).parent('label').addClass("checked");
+//    } else {
+//        $(this).parent('label').removeClass("checked");
+//    }
+//});
+
+
+
+
+function saveChanges() {
+var field = document.getElementById("form_pat");
+var save_btn = document.getElementById("save_btn");
+var save_btn_popup = document.getElementById("save_btn_popup");
+var clicked = false
+var clicked_field = false
+
+//document.addEventListener("click", function() {
+//    multiSelectUpdate();
+//})
+
+
+function form_saved(x) {
+x.addEventListener("click", function() {
+    clicked = true;
+    alert("Correctly saved");
+})
+
+}
+
+form_saved(save_btn)
+
+
+//onblur
+field.addEventListener("focusout", function(e) {
+
+    if (clicked == true) {
+
     } else {
-        $(this).parent('label').removeClass("checked");
-    }
+        console.log(e.relatedTarget);
+        if (!field.contains(e.relatedTarget)) {
+        togglePopup('save_form');
+        form_saved(save_btn_popup);
+        }
+};
 });
+
+}
+
+saveChanges()
+
+
