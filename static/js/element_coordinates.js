cfaf7079bd6b3316848cd1d7f1ebcaaa7ad33925
ivancapobianco <ivan.capobianco@yahoo.it>
2022-10-20 16:52:22 +0200

    scroll single columns depending on element_coordinates.js

--- static/js/element_coordinates.js
+++ static/js/element_coordinates.js
@@ -1,2 +1,19 @@
+function el_height_coordinates(x) {
+
+//var element = document.getElementById(x);
+var element = document.querySelector(x)
 var rect = element.getBoundingClientRect();
-console.log(rect.top, rect.right, rect.bottom, rect.left);
+
+console.log(rect.top, rect.bottom, rect.y)
+
+var height = 'calc(100vh - ' + rect.y.toString() + 'px - 10px)'
+
+element.style.setProperty(`height`, height);
+
+}
+
+//el_height_coordinates("documents_list")
+el_height_coordinates('.central.active .content')
+el_height_coordinates('.vtl')
+el_height_coordinates('.tasks-list')
+
